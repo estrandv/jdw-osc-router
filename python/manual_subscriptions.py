df30c2fa86416ed1827d68704d3c76fbff541be0
estrandv <>
2024-05-12 15:52:19 +0200

    Manual subscriptions

--- python/manual_subscriptions.py
+++ python/manual_subscriptions.py
@@ -7,6 +7,7 @@ client = udp_client.SimpleUDPClient("127.0.0.1", 13339)
 # jdw-sc subscriptions 
 jdw_sc_port = 13331
 jdw_seq_port = 14441
+keyboard_port = 17777
 client.send_message("/subscribe", ["/note_on_timed", "127.0.0.1", jdw_sc_port])
 client.send_message("/subscribe", ["/note_on", "127.0.0.1", jdw_sc_port])
 client.send_message("/subscribe", ["/bundle", "127.0.0.1", jdw_sc_port])
@@ -22,6 +23,15 @@ client.send_message("/subscribe", ["/bundle", "127.0.0.1", jdw_seq_port])
 client.send_message("/subscribe", ["/set_bpm", "127.0.0.1", jdw_seq_port])
 client.send_message("/subscribe", ["/hard_stop", "127.0.0.1", jdw_seq_port])
 client.send_message("/subscribe", ["/wipe_on_finish", "127.0.0.1", jdw_seq_port])
+
+client.send_message("/subscribe", ["/set_bpm", "127.0.0.1", keyboard_port])
+client.send_message("/subscribe", ["/keyboard_quantization", "127.0.0.1", keyboard_port])
+client.send_message("/subscribe", ["/keyboard_args", "127.0.0.1", keyboard_port])
+client.send_message("/subscribe", ["/keyboard_letter_index", "127.0.0.1", keyboard_port])
+client.send_message("/subscribe", ["/keyboard_mode_synth", "127.0.0.1", keyboard_port])
+client.send_message("/subscribe", ["/keyboard_mode_sampler", "127.0.0.1", keyboard_port])
+client.send_message("/subscribe", ["/keyboard_instrument_name", "127.0.0.1", keyboard_port])
+
 
 jdw_sample_port = 12367
 client.send_message("/subscribe", ["/play_sample", "127.0.0.1", jdw_sample_port])
