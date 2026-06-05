56322324858cb35a40ae188f9a460c601528658b
estrandv <emil.strandvik@gmail.com>
2026-06-05 21:13:17 +0200

    Add target/ to gitignore, remove from tracking

--- target/debug/deps/byteorder-7b2246451a749927.d
+++
@@ -1,7 +0,0 @@
-/home/estrandv/programming/jdw-osc-router/target/debug/deps/libbyteorder-7b2246451a749927.rmeta: /home/estrandv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs
-
-/home/estrandv/programming/jdw-osc-router/target/debug/deps/libbyteorder-7b2246451a749927.rlib: /home/estrandv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs
-
-/home/estrandv/programming/jdw-osc-router/target/debug/deps/byteorder-7b2246451a749927.d: /home/estrandv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs
-
-/home/estrandv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs:
--- target/debug/deps/jdw_osc_router-224a6cafc96841fd.d
+++
@@ -1,5 +0,0 @@
-/home/estrandv/programming/jdw-osc-router/target/debug/deps/jdw_osc_router-224a6cafc96841fd: src/main.rs
-
-/home/estrandv/programming/jdw-osc-router/target/debug/deps/jdw_osc_router-224a6cafc96841fd.d: src/main.rs
-
-src/main.rs:
